ecad68e7e6331672ef752c7586931163caf1c220
Laci Cartmell <57330789+laci-cartmell@users.noreply.github.com>
2022-11-03 21:06:02 -0500

    Update mydata.R

--- mydata.R
+++ mydata.R
@@ -1,9 +1,55 @@
 
+##  _____________________
+##
+## Script: NLCD_COLONY_PCA
+##
+## Purpose of script: Practice a PCA
+#   - Use any dataset that contains categorical and continuous variables
+#   - Read file into R, specify factor/categorical variables
+#   - Replace any NaN or Inf values with NA
+#   - Run a PCA using pcrcomp()
+#   - Create a screeplot and a biplot
+#   - Plot components of pcs with it colored by a categorical variable (Substrate, species, year)
+#   - Optional: Make a plot with 3 pricipal components and record the image
+#   
+#   
+## Included dataset:
+##    - found here: https://github.com/laci-cartmell/Cartmell_Lesson_PCA/blob/main/Cartmell_NLCD_Rlesson.xlsx, 
+##   is from my own research and it contains information on the name, substrate, year, and the proportion
+##   of the 11 land use categories w/in a 1km radius of multiple cliff swallow colony sites.
+##  
+## A PCA works well with datasets like this one as it has multicollinearity btw variables and 
+##  categorical variables that we want to understand how they influence the continuous variables.
+## But feel free to use your own data or any other datasets if at least 1 cat. var. you can group by
+## 
+##
+## Author: Laci Cartmell
+##
+## Date Created: 2022-11-03
+##
+##
+## _____________________
+## Notes: The below script is setup for Cartmell_NLCD_Rlesson.xlsx
+##        The top section of the script is preparing the dataset for the pca
+##        PCA is done in the bottom half
+##        If unable to save images, clips of images works okay too.
+##        
+## _____________________
+## Happy coding!
 
-##################### 
-# Practice with my work research data
-#####################
 
+## load the packages needed: 
+
+install.packages("ggfortify")
+install.packages("devtools")
+install.packages("remotes") 
+install.packages("rgl")
+install.packages("pca3d")
+
+##################
+#### PCA - Principal component analysis - 
+##################
+# 
 # read in data
 library(readxl)
 Cartmell_NLCD_Rlesson <- read_excel("Cartmell_NLCD_Rlesson.xlsx")
@@ -20,11 +66,14 @@ datatable$Substrate <- as.factor(datatable$Substrate)
 #contrasts(datatable$Substrate) <- contr.treatment(c(".1", ".2", ".3", ".4"), base=4)
 
 datatable$Year <- as.factor(datatable$Year)
-#contrasts(datatable$Year) <- contr.treatment(c("2001", "2004", "2006", "2008", "2013", "2016", "2019"), base=1)
+
+years <- c("2001", "2004", "2006", "2008", "2013", "2016", "2019")
+
 
 datatable$COLONYNAME = factor(datatable$COLONYNAME,
                               levels = unique(datatable$COLONYNAME))
 
+#check factor(datatable$var) worked
 summary(datatable)
 
 
@@ -34,17 +83,18 @@ all(is.na(datatable$Size))
 #Replace NaN & Inf with NA
 datatable[is.na(datatable) | datatable=="-Inf"] = NA
 
+#check for replacement
 summary(datatable)
+
+# Check correlation if you're feeling adventurous
+round(cor(iris[,1:4]), 2) #iris data
+round(cor(datatable[,5:11]), 2) #nlcd data
 
 
 ##################
 #### PCA - Principal component analysis - 
 ##################
 
-data <- datatable[,c(18:28)] # only continuous
-str(data)
-head(data)
-summary(data)
 
 data.pca <- prcomp(datatable[,c(5:11)], center = TRUE, scale. = TRUE)
 summary(data.pca)
@@ -52,6 +102,9 @@ summary(data.pca)
 
 #look at PCA object
 str(data.pca)
+
+#loadings
+data.pca
 
 #plotting pca
 # loading library
